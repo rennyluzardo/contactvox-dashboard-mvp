2201c61bfe1738a3f8f939c5f3dd475a7d8f6c7b
Renny <rluzardo@sidevox.com>
2020-06-29 00:01:27 -0500

    index page setup

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,118 @@
-import Head from 'next/head'
+import { Component } from "react";
+// import { connect } from "react-redux";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// Components
+import MainLayout from "../components/layouts/MainLayout";
+import SideHeader from "../components/global/SideHeader";
+import SideSearchbar from "../components/global/SideSearchbar";
+import ChatList from "../components/global/ChatList";
+import SideFooter from "../components/global/SideFooter";
+import SideHeaderDetail from "../components/global/SideHeaderDetail";
+import ChatHeader from "../components/chat/ChatHeader";
+import ChatHistory from "../components/chat/ChatHistory";
+import InputMessage from "../components/chat/InputMessage";
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const activeChat = [<ChatHeader />, <ChatHistory />, <InputMessage />];
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+const welcome = (
+  <div>
+    <h1>Bienvenido</h1>
+    <br />
+    <h6>Mostrar charts aqui..</h6>
+  </div>
+);
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+const homeSidebar = [
+  <SideHeader></SideHeader>,
+  <SideSearchbar></SideSearchbar>,
+  <ChatList></ChatList>,
+  <SideFooter></SideFooter>,
+];
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+const detailSidebar = <SideHeaderDetail></SideHeaderDetail>;
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+class Index extends Component {
+  constructor(props) {
+    super(props);
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    this.state = {};
+  }
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+  componentDidMount() {
+    $("body .messages").animate(
+      {
+        scrollTop: $(document).height(),
+      },
+      "fast"
+    );
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    $("#profile-img").click(function () {
+      $("#status-options").toggleClass("active");
+    });
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    $("body .expand-button").click(function () {
+      $("body #profile").toggleClass("expanded");
+      $("body #contacts").toggleClass("expanded");
+    });
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+    $("#status-options ul li").click(function () {
+      $("#profile-img").removeClass();
+      $("#status-online").removeClass("active");
+      $("#status-away").removeClass("active");
+      $("#status-busy").removeClass("active");
+      $("#status-offline").removeClass("active");
+      $(this).addClass("active");
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+      if ($("#status-online").hasClass("active")) {
+        $("#profile-img").addClass("online");
+      } else if ($("#status-away").hasClass("active")) {
+        $("#profile-img").addClass("away");
+      } else if ($("#status-busy").hasClass("active")) {
+        $("#profile-img").addClass("busy");
+      } else if ($("#status-offline").hasClass("active")) {
+        $("#profile-img").addClass("offline");
+      } else {
+        $("#profile-img").removeClass();
+      }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      $("#status-options").removeClass("active");
+    });
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+    $(".contact").click(function (e) {
+      console.log("chat #", $(this).data("id"));
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+      $("#frame .content").css("display", "block");
+    });
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+    $("body #configurations").click(function () {
+      $("body #bg-frame #sidepanel").css("transform", "translateX(0%)");
+    });
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+    $("#back").click(function () {
+      $("#bg-frame #sidepanel").removeAttr("style");
+    });
+  }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+  componentWillUnmount() {}
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+  render() {
+    const propsPrimaryLayout = {
+      frameId: "frame",
+      sideBar: homeSidebar,
+      displayContent: "none",
+    };
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+    const propsSecondaryLayout = {
+      frameId: "bg-frame",
+      sideBar: detailSidebar,
+      displayContent: "block",
+    };
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    return [
+      <MainLayout {...propsPrimaryLayout}>{activeChat}</MainLayout>,
+      <MainLayout {...propsSecondaryLayout}>{welcome}</MainLayout>,
+    ];
+  }
 }
+
+export default Index;
